4c067031982dd0bd18a86ccb25658495889681f1
Shahnawaz Hussain <shahnawazhussain2802@gmail.com>
2024-07-28 13:39:29 +0530

    fix: got api working with CORB and CORS

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import cv2
-import flask
 import numpy as np
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 from main import get_measurements_from_image
@@ -9,7 +8,7 @@ from main import get_measurements_from_image
 app = Flask(__name__)
 cors = CORS(app, origins="*")
 
-VERSION = "v-0.2.8"
+VERSION = "v-0.3.0"
 
 
 @app.route("/")
@@ -21,27 +20,29 @@ def hello_world():
 @app.route("/get-measurements", methods=["POST"])
 @cross_origin()
 def process_image():
-    print(request.files)
-    headers = {"Access-Control-Allow-Origin": "*", "X-Content-Type-Options": "nosniff"}
-    if "image" not in request.files:
-        return jsonify({"error": "No image file found in request"}), 400, headers
-    image_file = request.files["image"]
-    if not image_file.mimetype.startswith("image/"):
-        return jsonify({"error": "Invalid image format"}), 400, headers
-    image = cv2.imdecode(
-        np.fromstring(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED
-    )
-    measurements = get_measurements_from_image(image)
-    if measurements is None:
-        return (
-            jsonify(
-                {"error": "Could not detect dress in the image. Please re-upload."}
-            ),
-            400,
-            headers,
-        )
-    res = {}
-    for key in measurements:
-        res[key] = round(measurements[key]["distance"], 2)
-    print(res)
-    return jsonify(res), 200, headers
+    try:
+        if "image" not in request.files:
+            erres = {"error": "No image file found in request"}
+            return str(erres), 400
+
+        image_file = request.files["image"]
+        if not image_file.mimetype.startswith("image/"):
+            erres = {"error": "Invalid image format"}
+            return str(erres), 400
+
+        img_string = image_file.read()
+        image = cv2.imdecode(np.fromstring(img_string, np.uint8), cv2.IMREAD_UNCHANGED)
+
+        measurements = get_measurements_from_image(image)
+        if measurements is None:
+            erres = {"error": "Could not detect dress in the image. Please re-upload."}
+            return str(erres), 400
+
+        res = {}
+        for key in measurements:
+            res[key] = round(measurements[key]["distance"], 2)
+        return str(res), 200
+
+    except Exception as e:
+        erres = {"error": str(e)}
+        return str(erres), 500
