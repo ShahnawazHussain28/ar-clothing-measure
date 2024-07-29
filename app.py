7bbeb4737d2abb707267fab4f6a5116fcc322f69
Shahnawaz Hussain <shahnawazhussain2802@gmail.com>
2024-07-29 23:46:44 +0530

    commented body detection part

--- app.py
+++ app.py
@@ -46,38 +46,38 @@ def process_image():
         return str({"error": str(e)}), 500
 
 
-@app.route("/get-body-measurements", methods=["POST"])
-@cross_origin()
-def process_body_image():
-    try:
-        if (
-            "height" not in request.form
-            or "front" not in request.files
-            or "side" not in request.files
-        ):
-            return str({"error": "Missing required parameters"}), 401
-
-        height = float(request.form["height"])
-        front_img = request.files["front"]
-        side_img = request.files["side"]
-
-        if not front_img.mimetype.startswith("image/"):
-            return str({"error": "Invalid front image format"}), 402
-        if not side_img.mimetype.startswith("image/"):
-            return str({"error": "Invalid side image format"}), 402
-
-        front_string = np.fromfile(front_img, np.uint8)
-        side_string = np.fromfile(side_img, np.uint8)
-
-        front = cv2.imdecode(front_string, cv2.IMREAD_COLOR)
-        side = cv2.imdecode(side_string, cv2.IMREAD_COLOR)
-
-        measurements = get_body_measurements(height, front, side)
-        if "error" in measurements:
-            return str({"error": measurements["error"]}), 450
-
-        return str(measurements), 200
-
-    except Exception as e:
-        traceback.print_exc()
-        return str({"error": str(e)}), 500
+# @app.route("/get-body-measurements", methods=["POST"])
+# @cross_origin()
+# def process_body_image():
+#     try:
+#         if (
+#             "height" not in request.form
+#             or "front" not in request.files
+#             or "side" not in request.files
+#         ):
+#             return str({"error": "Missing required parameters"}), 401
+#
+#         height = float(request.form["height"])
+#         front_img = request.files["front"]
+#         side_img = request.files["side"]
+#
+#         if not front_img.mimetype.startswith("image/"):
+#             return str({"error": "Invalid front image format"}), 402
+#         if not side_img.mimetype.startswith("image/"):
+#             return str({"error": "Invalid side image format"}), 402
+#
+#         front_string = np.fromfile(front_img, np.uint8)
+#         side_string = np.fromfile(side_img, np.uint8)
+#
+#         front = cv2.imdecode(front_string, cv2.IMREAD_COLOR)
+#         side = cv2.imdecode(side_string, cv2.IMREAD_COLOR)
+#
+#         measurements = get_body_measurements(height, front, side)
+#         if "error" in measurements:
+#             return str({"error": measurements["error"]}), 450
+#
+#         return str(measurements), 200
+#
+#     except Exception as e:
+#         traceback.print_exc()
+#         return str({"error": str(e)}), 500
